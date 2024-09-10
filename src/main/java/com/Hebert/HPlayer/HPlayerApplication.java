c9f5409266ae71772b077a8e519404f4abb03ab0
heberttt <hebertp054@gmail.com>
2024-09-10 22:19:58 +0800

    first commit

---
+++ src/main/java/com/Hebert/HPlayer/HPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.Hebert.HPlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HPlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Hebert/HPlayer/HPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hebert.HPlayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
